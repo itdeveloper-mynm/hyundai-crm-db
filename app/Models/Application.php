929f5f4a90ce083f9fb9415ae9ab88e87e5ac921
ATEEBNISAAR <ateebnisar19.an@gmail.com>
2025-03-26 11:36:04 +0500

    target & campagin percentage & performance & target graph

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -659,7 +659,7 @@ class Application extends Model
 
                 return [
                     'campaign_id' => $application->campaign_id,
-                    'percentage' => $application->campaign->percentage,
+                    'percentage' => $application->campaign->percentage ?? 0,
                     'campaign_name' => $application->campaign->name ?? 'Others',
                     'mql' => $application->mql,
                     'cql' => $application->cql,
